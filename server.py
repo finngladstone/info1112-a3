6624fc709bc000e017008ed4526f1acad73236b5
Finn Gladstone <Finngladstone9@gmail.com>
2022-10-13 14:32:02 +1100

    Added base files

---
+++ server.py
@@ -0,0 +1,17 @@
+import os
+import socket
+import sys
+
+
+# Visit https://edstem.org/au/courses/8961/lessons/26522/slides/196175 to get
+PERSONAL_ID = '506D1D'
+PERSONAL_SECRET = 'ca1316f53ed206e3217d41b3951fffa5'
+
+
+def main():
+    # TODO
+    pass
+
+
+if __name__ == '__main__':
+    main()
